ac3126e17846ad8b3ce071b5bd33215d89f9d958
Yannik Rudolph <yannik.rudolph@conceptpeople.de>
2020-12-08 18:40:22 +0100

    Initial commit with project setup

---
+++ product-organizer-backened/src/main/java/com/rudolph/productorganizer/backened/ProductOrganizerBackenedApplication.java
@@ -0,0 +1,13 @@
+package com.rudolph.productorganizer.backened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrganizerBackenedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrganizerBackenedApplication.class, args);
+	}
+
+}
---
+++ product-organizer-backened/src/test/java/com/rudolph/productorganizer/backened/ProductOrganizerBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudolph.productorganizer.backened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrganizerBackenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
