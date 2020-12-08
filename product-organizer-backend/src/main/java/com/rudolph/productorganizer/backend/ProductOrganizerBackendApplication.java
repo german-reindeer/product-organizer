fb24b9eaee3c10dc6eef2eb8f616c329f658af6f
Yannik Rudolph <yannik.rudolph@conceptpeople.de>
2020-12-08 18:49:28 +0100

    Improved project setup with NX and correct backend project name

---
+++ product-organizer-backend/src/main/java/com/rudolph/productorganizer/backend/ProductOrganizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rudolph.productorganizer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrganizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrganizerBackendApplication.class, args);
+	}
+
+}
---
+++ product-organizer-backend/src/test/java/com/rudolph/productorganizer/backend/ProductOrganizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudolph.productorganizer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrganizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
